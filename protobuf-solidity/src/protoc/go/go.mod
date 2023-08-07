aa852266590bc3f4c108a42e5561e074d8847ef8
Fatih <fatihfurkn@gmail.com>
2023-08-07 17:51:59 +0300

    update protos

--- protobuf-solidity/src/protoc/go/go.mod
+++ protobuf-solidity/src/protoc/go/go.mod
@@ -2,7 +2,6 @@ module github.com/datachainlab/solidity-protobuf/protobuf-solidity/src/protoc/go
 
 go 1.17
 
-require (
-	github.com/golang/protobuf v1.5.2
-	google.golang.org/protobuf v1.27.1
-)
+require google.golang.org/protobuf v1.30.0
+
+require github.com/google/go-cmp v0.5.9 // indirect
--- protobuf-solidity/src/protoc/plugin/solidity_protobuf_extensions_pb2.py
+++ protobuf-solidity/src/protoc/plugin/solidity_protobuf_extensions_pb2.py
@@ -4,9 +4,8 @@
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import message as _message
-from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
+from google.protobuf.internal import builder as _builder
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -17,23 +16,14 @@ from google.protobuf import descriptor_pb2 as google_dot_protobuf_dot_descriptor
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\"solidity-protobuf-extensions.proto\x12\x08solidity\x1a google/protobuf/descriptor.proto\"\x1f\n\x0b\x46ileOptions\x12\x10\n\x08location\x18\x01 \x01(\t:K\n\x0c\x66ile_options\x12\x1c.google.protobuf.FileOptions\x18\xd0\x86\x03 \x01(\x0b\x32\x15.solidity.FileOptionsBTZRgithub.com/datachainlab/solidity-protobuf/protobuf-solidity/src/protoc/go;solidityb\x06proto3')
 
-
-FILE_OPTIONS_FIELD_NUMBER = 50000
-file_options = DESCRIPTOR.extensions_by_name['file_options']
-
-_FILEOPTIONS = DESCRIPTOR.message_types_by_name['FileOptions']
-FileOptions = _reflection.GeneratedProtocolMessageType('FileOptions', (_message.Message,), {
-  'DESCRIPTOR' : _FILEOPTIONS,
-  '__module__' : 'solidity_protobuf_extensions_pb2'
-  # @@protoc_insertion_point(class_scope:solidity.FileOptions)
-  })
-_sym_db.RegisterMessage(FileOptions)
-
+_globals = globals()
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'solidity_protobuf_extensions_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
   google_dot_protobuf_dot_descriptor__pb2.FileOptions.RegisterExtension(file_options)
 
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'ZRgithub.com/datachainlab/solidity-protobuf/protobuf-solidity/src/protoc/go;solidity'
-  _FILEOPTIONS._serialized_start=82
-  _FILEOPTIONS._serialized_end=113
+  _globals['_FILEOPTIONS']._serialized_start=82
+  _globals['_FILEOPTIONS']._serialized_end=113
 # @@protoc_insertion_point(module_scope)
